b2d9cdfa6121ab6a0610f526c1dc82e89857d87f
tmjishan <tmjishan@gmail.com>
2025-07-09 21:23:17 +0600

    service clean page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,4 +1,3 @@
-import CenterSlider from "@/components/ProductivitySlider";
 import { fetchServicesData } from "@/lib/graphql-client";
 
 export default async function ServicePage() {
@@ -12,31 +11,5 @@ export default async function ServicePage() {
     );
   }
 
-  const imageUrl = data.map((item: any) => {
-    const serviceIcon = item.serviceDetails?.serviceIcon?.node?.sourceUrl;
-    return {
-      url: serviceIcon || "",
-      title: item.serviceDetails?.serviceTitle || "Service Image",
-      description: item.serviceDetails?.shortDescription || "No description",
-    };
-  });
-
-  return (
-    <>
-      <div className="text-center text-yellow-700">
-        <CenterSlider
-          slides={imageUrl.map((item) => {
-            return {
-              image: item.url,
-              title: item.title,
-              description: item.description,
-              buttonText: "Learn More",
-              buttonUrl: "#", // Replace with actual URL if needed
-            };
-          })}
-        />
-      </div>
-      <div>Hello SERVICE</div>
-    </>
-  );
+  return <div>Hello SERVICE</div>;
 }
